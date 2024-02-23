1327c827611c1bdab59b3aa225c0fa2e6850c550
julius <nyerereoffice@gmail.com>
2024-02-23 17:11:49 +0300

    Implemented user-specific task view feature

--- TasksProject/Tasks1/urls.py
+++ TasksProject/Tasks1/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name ='Tasks1'
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('user/<int:user_id>/', views.user_tasks, name='user_tasks'),
     path('Tasks/<str:pk>/',views.Task, name = "ToDo"),
     path('addTask/', views.addTask, name = 'addTask'),
     path('addUser/', views.addUser, name = 'addUser'),
+    
 ]
--- TasksProject/Tasks1/views.py
+++ TasksProject/Tasks1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect, get_object_or_404
 from django.http import HttpResponse 
 from .forms import AddForm, AddUser
 from .models import User, ToDo
@@ -33,19 +33,21 @@ def addTask(request):
 
 
 def addUser(request):
-   
-    if (request.method == 'POST'):
+    if request.method == 'POST':
         form = AddUser(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/Tasks1')
-        else:
-            form = AddUser()
+            return redirect('/Tasks')
+    else:
+        form = AddUser()
 
-    context = {'form': AddUser(), 'Created':Created}
+    context = {'form': form, 'Created': Created}
     return render(request, 'Tasks1/addTask.html', context)
 
 
-    
+def user_tasks(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    tasks = user.todo_set.all()  
+    return render(request, 'Tasks1/userTask.html', {'user': user, 'tasks': tasks})
 
 
